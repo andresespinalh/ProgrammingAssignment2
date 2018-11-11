c89247a91f0ed5da6b48a1685c983a9957928c8f
Andrés Espinal <andres.espinalh@gmail.com>
2018-11-10 22:43:39 -0600

    Create logic to cache matrix inversion calculations

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions create a special kind of matrix
+## that enables R to cache the result of a matrix inversion (Which is computationaly expensive)
 
-## Write a short comment describing this function
+## This function gets a matrix as an input and returns a list of functions 
+## that handle the matrix values and the inverted matrix values by using lexical scoping
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The following function receives a object created using makeCachaMatrix( x = matrix() )
+## and checks wether an inversion on that matrix occured previously.
+## If it was calculated before, it returns the cached value; otherwise it performs the calculation, caches it and returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
